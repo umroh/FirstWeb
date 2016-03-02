6b72d72af111ae5f689acac17c57a9b7654848d1
Umroh.machfudza <umroh.machfudza@ui.ac.id>
2016-03-02 15:57:19 +0700

    percobaan Log

--- prototypeAsisten/wsgi.py
+++ prototypeAsisten/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prototypeAsisten.settings")
 
 application = get_wsgi_application()
+
+path = '/templates/home.html'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
